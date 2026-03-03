2c42ac98a60f1f2bbd7247e27fbd3ac985354e6c
Adnan Siddiqui <adnan0977@gmail.com>
2026-03-03 18:41:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-13276853-31fe1",
+  "appId": "1:746245211113:web:420effb58c04834b8d8178",
+  "apiKey": "AIzaSyAX7cidULlAS6iu0tctSKIYr3Jm7E1aKXg",
+  "authDomain": "studio-13276853-31fe1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "746245211113"
+};
