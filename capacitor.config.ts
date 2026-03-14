ab5f87ba82cc7db3a696480b8dcdc90a44adb2ce
adnan0977 <adnan0977@gmail.com>
2026-03-14 16:25:53 +0530

    commit from AS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.vlognest.app',
-  appName: 'VlogNest',
+  appId: 'com.islamopedia.app',
+  appName: 'Islamopedia',
   webDir: 'out',
   bundledWebRuntime: false,
   server: {
